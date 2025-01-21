0cd138cf757bc2f1890ace0994c60f0b0a5f1056
Boris Brangeon <boris.brangeon@plateforme-tipee.com>
2025-01-21 16:02:48 +0100

    modif lcz

--- pymdu/geometric/Lcz.py
+++ pymdu/geometric/Lcz.py
@@ -40,47 +40,47 @@ class Lcz(GeoCore):
         set_crs: int = None,
     ):
         """
-                Initializes the object with the given parameters.
+        Initializes the object with the given parameters.
 
-                Args:
-                    filepath_shp (str): The file path to the shapefile.
-                    output_path (str): The output path for the processed data. If not provided, a default temporary path will be used.
-                    set_crs (int): The CRS (Coordinate Reference System) to be set.
+        Args:
+            filepath_shp (str): The file path to the shapefile.
+            output_path (str): The output path for the processed data. If not provided, a default temporary path will be used.
+            set_crs (int): The CRS (Coordinate Reference System) to be set.
 
-                Example:
-                    ```python exec="true" source="tabbed-right" html="1" tabs="Source code|Plot"
-                    import matplotlib.pyplot as plt
-                    import matplotlib.patches as mpatches
+        Example:
+            ```python exec="true" source="tabbed-right" html="1" tabs="Source code|Plot"
+            import matplotlib.pyplot as plt
+            import matplotlib.patches as mpatches
 
-                    plt.clf()  # markdown-exec: hide
-                    import pymdu.geometric.Lcz as Lcz
-        $
-                    lcz = Lcz()
-                    lcz_gdf = lcz.run().to_gdf()
-                    lcz.bbox = [-1.152704, 46.181627, -1.139893, 46.18699]
-                    table_color = lcz.table_color
-                    fig, ax = plt.subplots(figsize=(10, 10))
-                    lcz_gdf.plot(ax=ax, edgecolor=None, color=lcz_gdf['color'])
-                    patches = [
-                        mpatches.Patch(color=info[1], label=info[0])
-                        for info in table_color.values()
-                    ]
-                    plt.legend(
-                        handles=patches,
-                        loc='upper right',
-                        title='LCZ Legend',
-                        bbox_to_anchor=(1.1, 1.0),
-                    )
-                    from io import StringIO  # markdown-exec: hide
+            plt.clf()  # markdown-exec: hide
+            import pymdu.geometric.Lcz as Lcz
 
-                    buffer = StringIO()  # markdown-exec: hide
-                    plt.gcf().set_size_inches(10, 5)  # markdown-exec: hide
-                    plt.savefig(buffer, format='svg', dpi=199)  # markdown-exec: hide
-                    print(buffer.getvalue())  # markdown-exec: hide
-                    ```
+            lcz = Lcz()
+            lcz_gdf = lcz.run().to_gdf()
+            lcz.bbox = [-1.152704, 46.181627, -1.139893, 46.18699]
+            table_color = lcz.table_color
+            fig, ax = plt.subplots(figsize=(10, 10))
+            lcz_gdf.plot(ax=ax, edgecolor=None, color=lcz_gdf['color'])
+            patches = [
+                mpatches.Patch(color=info[1], label=info[0])
+                for info in table_color.values()
+            ]
+            plt.legend(
+                handles=patches,
+                loc='upper right',
+                title='LCZ Legend',
+                bbox_to_anchor=(1.1, 1.0),
+            )
+            from io import StringIO  # markdown-exec: hide
 
-                Todo:
-                    * For module TODOs
+            buffer = StringIO()  # markdown-exec: hide
+            plt.gcf().set_size_inches(10, 5)  # markdown-exec: hide
+            plt.savefig(buffer, format='svg', dpi=199)  # markdown-exec: hide
+            print(buffer.getvalue())  # markdown-exec: hide
+            ```
+
+        Todo:
+            * For module TODOs
         """
         super().__init__()
         self.filepath_shp = filepath_shp
