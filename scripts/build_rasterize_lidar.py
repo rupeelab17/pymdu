fe632d0ab59384be33cbfefd48ff7d1374416013
Boris Brangeon <boris.brangeon@plateforme-tipee.com>
2025-06-05 13:31:38 +0200

    add lib win rasterize

--- scripts/build_rasterize_lidar.py
+++ scripts/build_rasterize_lidar.py
@@ -1,14 +1,20 @@
 # from pybind11.setup_helpers import Pybind11Extension
+import sys
 from typing import Any, Dict
 
 from setuptools_cpp import ExtensionBuilder, Pybind11Extension
 
 print("Building rasterize for lidar")
 
+compile_args = []
+if sys.platform == "win32":
+    compile_args = ['/O2', '/std:c++17']
+else:
+    compile_args = ['-O3', '-pthread', '-Wall', '-shared', '-std=c++11', '-undefined','dynamic_lookup']
+
 ext_modules = [
     Pybind11Extension("pymdu.image.rasterize", ["pymdu/image/lib.cpp"],
-                      extra_compile_args=['-O3', '-pthread', '-Wall', '-shared', '-std=c++11', '-undefined',
-                                          'dynamic_lookup'],
+                      extra_compile_args=compile_args,
                       language='c++'),  # mypycify([
     #     '--disallow-untyped-defs',  # Pass a mypy flag
     #     'pymdu/__init__.py',
