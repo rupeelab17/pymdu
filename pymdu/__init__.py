60fb22444ac37efdef84adb67fff2c270b7a4e13
Boris Brangeon <boris.brangeon@plateforme-tipee.com>
2025-07-04 10:19:26 +0200

    maj pymdu/diagnostic/Diagnostic.py

--- pymdu/__init__.py
+++ pymdu/__init__.py
@@ -6,5 +6,6 @@ from pymdu import physics
 from pymdu import plotting
 from pymdu import image
 from pymdu import meteo
+from pymdu import diagnostic
 
-__all__ = ['collect', 'demos', 'physics', 'geometric', 'plotting', 'image', 'meteo']
+__all__ = ['collect', 'demos', 'physics', 'geometric', 'plotting', 'image', 'meteo', 'diagnostic']
